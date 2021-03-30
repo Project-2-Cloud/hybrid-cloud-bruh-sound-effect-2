3903d3f9cb675c9f587140b1984c2d3914312d45
pietergmail <33780619+pietergmail@users.noreply.github.com>
2021-03-30 16:19:51 +0200

    pretty sure i finished this shit.

--- src/main/java/psb/webshop/prototype/product/ProductdbSql.java
+++ src/main/java/psb/webshop/prototype/product/ProductdbSql.java
@@ -38,24 +38,71 @@ public class ProductdbSql implements ProductDb{
     }
 
 
+
     @Override
     public List<Product> getAll() {
         ResultSet result = null;
         ArrayList list = new ArrayList();
         try (Connection connection = DriverManager.getConnection(url, properties); Statement statement = connection.createStatement()) {
+            result = statement.executeQuery("SELECT * FROM \"usb\"");
+            list = (ArrayList) makePrdouctsFromSet(result);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return null;
+        return list;
     }
 
     @Override
     public void add(Product product) {
-
+        //not yet implemented
     }
 
     @Override
     public void update(Product product) {
+        //not yet implemented
+    }
 
+    private Product makeProdFromSet(ResultSet result) {
+        Product p = null;
+        try{
+            String id = result.getString("id");
+            String title = result.getString("title");
+            String price = result.getString("price");
+            String description = result.getString("description");
+            String quantity = result.getString("quantity");
+            String thumbnail_url = result.getString("thumbnail_url");
+            try{
+                p = new Product(id, title, Double.parseDouble(price), description, thumbnail_url, Integer.parseInt(quantity));
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return p;
+    }
+
+    private List<Product> makePrdouctsFromSet(ResultSet result){
+        ArrayList<Product> products = new ArrayList<>();
+        Product p = null;
+
+        try{
+            while(result.next()){
+                String id = result.getString("id");
+                String title = result.getString("title");
+                String price = result.getString("price");
+                String description = result.getString("description");
+                String quantity = result.getString("quantity");
+                String thumbnail_url = result.getString("thumbnail_url");
+                try{
+                    p = new Product(id, title, Double.parseDouble(price), description, thumbnail_url, Integer.parseInt(quantity));
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return products;
     }
 }
