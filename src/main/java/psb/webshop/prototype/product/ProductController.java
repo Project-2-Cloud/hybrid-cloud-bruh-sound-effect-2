f5182ebf30fe9f3fe66268dc3579b771108d4898
pietergmail <33780619+pietergmail@users.noreply.github.com>
2021-03-31 12:45:11 +0200

    some refactoring

--- src/main/java/psb/webshop/prototype/product/ProductController.java
+++ src/main/java/psb/webshop/prototype/product/ProductController.java
@@ -18,31 +18,5 @@ public class ProductController {
     @RequestMapping("/products")
     public List<Product> getAllProducts() {
         return (List<Product>) service.getAll();
-        /*return Arrays.asList(
-                new Product(
-                        "1",
-                        "Ducky USB",
-                        12.5,
-                        "Ducky USB uses 2.0 technology, up to 50 passwords.",
-                        "https://i.imgur.com/bEwVYmD.jpg",
-                        500
-                ),
-                new Product(
-                        "2",
-                        "Rudi USB",
-                        15,
-                        "Ducky USB uses 3.0 generation 1 technology, up to 100 passwords.",
-                        "https://i.imgur.com/bEwVYmD.jpg",
-                        250
-                ),
-                new Product(
-                        "3",
-                        "IBM Stick",
-                        17.5,
-                        "Ducky USB uses 3.0 generation 2 technology, up to 150 passwords.",
-                        "https://i.imgur.com/bEwVYmD.jpg",
-                        125
-                )
-        );*/
     }
 }
