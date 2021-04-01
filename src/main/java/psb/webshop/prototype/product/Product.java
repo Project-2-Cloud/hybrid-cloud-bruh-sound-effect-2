b99db9977c483dc138d890cebd5a1c9dbb9112ef
ghostek99 <szymon.nidecki@student.ucll.be>
2021-04-01 10:13:49 +0200

    Reverted

--- src/main/java/psb/webshop/prototype/product/Product.java
+++ src/main/java/psb/webshop/prototype/product/Product.java
@@ -1,6 +1,5 @@
 package psb.webshop.prototype.product;
 
-import javax.annotation.processing.Generated;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
--- src/main/java/psb/webshop/prototype/product/ProductController.java
+++ src/main/java/psb/webshop/prototype/product/ProductController.java
@@ -1,14 +1,15 @@
 package psb.webshop.prototype.product;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import psb.webshop.prototype.Service.ProductService;
 
-import java.util.List;
-
-@CrossOrigin(origins = "http://frontend-ucllteam11.ocp-ucll-40cb0df2b03969eabb3fac6e80373775-0000.eu-de.containers.appdomain.cloud/", allowedHeaders = "*")
+@CrossOrigin(origins = "http://localhost:8080", allowCredentials = "true")
 @RestController
 public class ProductController {
 
