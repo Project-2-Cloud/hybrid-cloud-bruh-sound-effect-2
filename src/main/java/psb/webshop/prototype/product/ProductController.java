b0d5cc80ea79eb73108aee6863d1bb5fcf01c329
ghostek99 <szymon.nidecki@student.ucll.be>
2021-04-01 11:49:33 +0200

    Changed origin url

--- src/main/java/psb/webshop/prototype/product/ProductController.java
+++ src/main/java/psb/webshop/prototype/product/ProductController.java
@@ -3,20 +3,20 @@ package psb.webshop.prototype.product;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import psb.webshop.prototype.Service.ProductService;
 
-@CrossOrigin(origins = "http://localhost:8080", allowCredentials = "true")
+@CrossOrigin(origins = "http://frontend-ucllteam11.ocp-ucll-40cb0df2b03969eabb3fac6e80373775-0000.eu-de.containers.appdomain.cloud/", allowCredentials = "true")
 @RestController
 public class ProductController {
 
     @Autowired
     private ProductService service;
 
-    @RequestMapping("/products")
+    @GetMapping("/products")
     public List<Product> getAllProducts() {
         return (List<Product>) service.getAll();
     }
